80e1af8b7709574efc8b05ef2eb897bd6504056c
Xu <xulin06@baidu.com>
2020-10-15 19:33:34 +0800

    dom节点变换修改成rang变换，完成setState方法

--- main.js
+++ main.js
@@ -5,17 +5,28 @@ import {
 } from './toy-react';
 
 class MyComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            a: 1,
+            b: 2
+        };
+    }
     render() {
         return <div>
-            <h1>my component</h1>
+            <h1 id="a">my component</h1>
+            <button onClick={() => {
+                this.setState({ a: this.state.a++ });
+            }}>click</button>
+            <span>{this.state.a.toString()}</span>
             {this.children}
         </div>;
     }
 }
 let component = <MyComponent id="d">
-<span>abc</span>
-<span></span>
-<span></span>
+    <span>abc</span>
+    <span></span>
+    <span></span>
 </MyComponent>;
 window.a = render(component, document.body);
 
--- toy-react.js
+++ toy-react.js
@@ -1,18 +1,37 @@
+const RENDER_TO_DOM = Symbol('renderToDom');
 class ElementWraper {
     constructor(tagName) {
         this.root = document.createElement(tagName);
     }
     setAttribute(name, value) {
-        this.root.setAttribute(name, value);
+         // 如果属性为onClick 单独识别
+        // 绑定事件，建立监听方法
+        if (name.match(/^on([\s\S]+)$/)) {
+            this.root.addEventListener(RegExp.$1.toLowerCase(), value);
+        }
+        else {
+            this.root.setAttribute(name, value);
+        }
     }
     appendChild(elem) {
-        this.root.appendChild(elem.root);
+        let range = document.createRange();
+        range.setStart(this.root, this.root.childNodes.length);
+        range.setEnd(this.root, this.root.childNodes.length);
+        elem[RENDER_TO_DOM](range);
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 
 }
 class TextWraper {
     constructor(content) {
         this.root = document.createTextNode(content);
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 }
 // 组件基类
@@ -22,6 +41,8 @@ export class Component {
         this._root = null;
         this.children = [];
         this.props = Object.create(null);
+        this._range = null;
+        this.state = null;
     }
     setAttribute(name, value) {
         this.props[name] = value;
@@ -29,16 +50,46 @@ export class Component {
     appendChild(component) {
         this.children.push(component);
     }
-    // root属性获取方法
-    get root() {
-        if (!this._root) {
-            // 子类获取root不得 会调用自身render方法
-            // render方法执行返回一段jsx语法 经过编译会自动调用createElement方法
-            // 如果createElement方法第一个参数为组件那么继续递归，直到找到第一个参数为tagName的情况为止
-            // 返回TextWraper 或者 ElementWraper类的实例为止
-            this._root = this.render().root;
+    // 打破root的思路使用range进行dom插入
+    // 私有方法
+    [RENDER_TO_DOM](range) {
+        this._range = range;
+        range.deleteContents();
+        range.insertNode(this.render().root);
+    }
+    // // root属性获取方法
+    // get root() {
+    //     if (!this._root) {
+    //         // 子类获取root不得 会调用自身render方法
+    //         // render方法执行返回一段jsx语法 经过编译会自动调用createElement方法
+    //         // 如果createElement方法第一个参数为组件那么继续递归，直到找到第一个参数为tagName的情况为止
+    //         // 返回TextWraper 或者 ElementWraper类的实例为止
+    //         this._root = this.render().root;
+    //     }
+    //     return this._root;
+    // }
+    rerender() {
+        this._range.deleteContents();
+        this[RENDER_TO_DOM](this._range);
+    }
+    setState(newState) {
+        if (this.state === null || typeof this.state !== 'object') {
+            this.state = newState;
+            this.rerender();
+            return;
         }
-        return this._root;
+        let merge = function (oldState, newState) {
+            for (let item in oldState) {
+                if (oldState === null || typeof oldState !== 'object') {
+                    newState[item] = oldState[item];
+                }
+                else {
+                    merge(oldState[item], newState[item]);
+                }
+            }
+        };
+        merge(this.state, newState);
+        this.rerender();
     }
 }
 export function createElement(tag, attributes, ...children) {
@@ -50,7 +101,7 @@ export function createElement(tag, attributes, ...children) {
         dom = new tag();
     }
     for (let attr in attributes) {
-        dom[attr] = attributes[attr];
+        dom.setAttribute(attr, attributes[attr]);
     }
     // 递归插入元素（数组）
     let insertChildren = children => {
@@ -74,5 +125,11 @@ export function createElement(tag, attributes, ...children) {
 }
 
 export function render(component, parentComponent) {
-    parentComponent.appendChild(component.root);
+    // render 方法并不是只有appendChild一种情况，同时需要位置信息
+    // dom api中range api是跟位置相关的
+    // parentComponent.appendChild(component.root);
+    let range = document.createRange();
+    range.setStart(parentComponent, 0);
+    range.setEnd(parentComponent, parentComponent.childNodes.length);
+    component[RENDER_TO_DOM](range);
 }
